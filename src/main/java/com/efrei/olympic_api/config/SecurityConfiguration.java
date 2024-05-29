4c6309526b29c878b1aec439a2119fe94f6e8366
Mathieu GUILLEMIN <mathieu.guillemin@sfl.com>
2024-05-29 12:12:30 +0200

    dockerize app and added swagger documentation

--- src/main/java/com/efrei/olympic_api/config/SecurityConfiguration.java
+++ src/main/java/com/efrei/olympic_api/config/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration {
         http.csrf(csrf -> csrf
                 .disable())
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/auth/**")
+                        .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
